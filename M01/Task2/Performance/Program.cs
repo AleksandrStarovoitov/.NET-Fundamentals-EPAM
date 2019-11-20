dc6665d1f1d5cc764282d18717b70e72101f9d2d
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2019-11-20 22:28:51 +0300

    Complete Task2

--- M01/Task2/Performance/Program.cs
+++ M01/Task2/Performance/Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Performance
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static Random rnd;
+        private const int ArraySize = 100000;
+
+        private static void Main(string[] args)
         {
+            rnd = new Random();
+
+            C[] classes = null;
+            S[] structs = null;
+
+            var classDelta = GetPrivateMemoryDelta(() => {
+                classes = new C[ArraySize];
+
+                for (int i = 0; i < ArraySize; i++)
+                {
+                    classes[i] = new C(rnd.Next(Int32.MaxValue));
+                }
+            });
+
+            var structDelta = GetPrivateMemoryDelta(() => {
+                structs = new S[ArraySize];
+
+                for (int i = 0; i < ArraySize; i++)
+                {
+                    structs[i].I = rnd.Next(Int32.MaxValue);
+                }
+            });
+
+            Console.WriteLine($"Class delta - struct delta: {classDelta - structDelta}");
+
+            var structSortTime = GetExecutionTime(() =>
+            {
+                Array.Sort<S>(structs);
+            });
+            Console.WriteLine($"Struct sort time: {structSortTime}");
+
+            var classSortTime = GetExecutionTime(() =>
+            {
+                Array.Sort<C>(classes);
+            });
+            Console.WriteLine($"Class sort time: {classSortTime}");
+        }
+
+        private static long GetPrivateMemoryDelta(Action action)
+        {
+            using (var procBefore = Process.GetCurrentProcess())
+            {
+                var memBefore = procBefore.PrivateMemorySize64;
+
+                action();
+
+                using (var procAfter = Process.GetCurrentProcess())
+                {
+                    var memAfter = procAfter.PrivateMemorySize64;
+                    var delta = memAfter - memBefore;
+                    Console.WriteLine($"PrivateMemorySize64 delta: {delta}");
+                    return delta;
+                }
+            }
+        }
+
+        private static long GetExecutionTime(Action action)
+        {
+            var watch = Stopwatch.StartNew();
+            action();
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
         }
     }
 }
