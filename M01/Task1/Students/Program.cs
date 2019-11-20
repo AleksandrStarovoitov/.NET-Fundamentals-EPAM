548f9cdeabc00208a647873aa10f9700e1698860
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2019-11-20 14:00:46 +0300

    Complete Task1

--- M01/Task1/Students/Program.cs
+++ M01/Task1/Students/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Students
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static Random rnd;
+
+        private static void Main(string[] args)
         {
+            var subjects = new[] {"Maths", "PE", "History", "Art"};
+
+            var students = new[]
+            {
+                new Student("Vasya Pupkin", "vasya.pupkin@epam.com"),
+                new Student("Ivan Ivanov", "ivan.ivanov@epam.com"),
+                new Student("Vlad Sokolov", "vlad.sokolov@epam.com"),
+
+                new Student("vasya.pupkin@epam.com"),
+                new Student("ivan.ivanov@epam.com"),
+                new Student("vlad.sokolov@epam.com"),
+            };
+
+            var studentSubjectDict = new Dictionary<Student, string[]>();
+
+            rnd = new Random();
+            foreach (var student in students)
+            {
+                studentSubjectDict[student] = new[]
+                {
+                    subjects[rnd.Next(subjects.Length)],
+                    subjects[rnd.Next(subjects.Length)],
+                    subjects[rnd.Next(subjects.Length)],
+                };
+            }
         }
     }
 }
---
+++ M01/Task1/Students/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Students
+{
+    class Student
+    {
+        private string email;
+
+        public string Name { get; set; }
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (!GetNameFromEmail(value).Equals(this.Name))
+                {
+                    throw new ArgumentException("Email does not match the name", nameof(email));
+                }
+
+                this.email = value;
+            }
+        }
+
+        public Student(string name, string email)
+        {
+            if (email == null)
+            {
+                throw new ArgumentNullException(nameof(email));
+            }
+
+            this.Name = name ?? GetNameFromEmail(email);
+            this.Email = email;
+        }
+        
+        public Student(string email) : this(null, email) { }
+        
+        private static string GetNameFromEmail(string email)
+        {
+            var splitEmail = email.Substring(0, email.IndexOf('@'))
+                                  .Split('.');
+            var firstName = splitEmail[0];
+            var secondName = splitEmail[1];
+
+            return firstName.First().ToString().ToUpper() + firstName.Substring(1) + ' ' +
+                   secondName.First().ToString().ToUpper() + secondName.Substring(1);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student student))
+            {
+                return false;
+            }
+
+            return ((student.Name == this.Name) && (student.Email == this.Email));
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked 
+            {
+                var hash = 17;
+                hash = hash * 23 + Name.GetHashCode();
+                hash = hash * 23 + Email.GetHashCode();
+
+                return hash;
+            }
+        }
+    }
+}
