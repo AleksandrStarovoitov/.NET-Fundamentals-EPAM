5ec6770c9793db61749ed963e3e775e7dd760359
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2019-11-22 12:25:54 +0300

    Move name and email strings to variables

--- M02/Task1/Students/Program.cs
+++ M02/Task1/Students/Program.cs
@@ -13,15 +13,22 @@ namespace Students
             {
                 var subjects = new[] { "Maths", "PE", "History", "Art" };
 
+                var vasyaPupkinName = "Vasya Pupkin";
+                var ivanIvanovName = "Ivan Ivanov";
+                var vladSokolovName = "Vlad Sokolov";
+                var vasyaPupkinEmail = "vasya.pupkin@epam.com";
+                var ivanIvanovEmail = "ivan.ivanov@epam.com";
+                var vladSokolovEmail = "vlad.sokolov@epam.com";
+
                 var students = new[]
                 {
-                    new Student("vasya.pupkin@epam.com", "Vasya Pupkin"),
-                    new Student("ivan.ivanov@epam.com", "Ivan Ivanov"),
-                    new Student("vlad.sokolov@epam.com", "Vlad Sokolov"),
+                    new Student(vasyaPupkinEmail, vasyaPupkinName),
+                    new Student(ivanIvanovEmail, ivanIvanovName),
+                    new Student(vladSokolovEmail, vladSokolovName),
 
-                    new Student("vasya.pupkin@epam.com"),
-                    new Student("ivan.ivanov@epam.com"),
-                    new Student("vlad.sokolov@epam.com"),
+                    new Student(vasyaPupkinEmail),
+                    new Student(ivanIvanovEmail),
+                    new Student(vladSokolovEmail),
                 };
 
                 var studentSubjectDict = new Dictionary<Student, string[]>();
