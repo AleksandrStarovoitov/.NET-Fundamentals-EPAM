a1bb4feb1fc4bb7b610efa4f9be71d2f1d2834f7
Aleksandr Starovoytov <starovoytov.it@mail.ru>
2019-11-23 14:10:51 +0300

    Add double char method

--- M03/Task1/M03_Task1/DoubleChars.cs
+++ M03/Task1/M03_Task1/DoubleChars.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace M03_Task1
 {
-    class DoubleChars
+    internal class DoubleChars
     {
+        public static string GetDoubleCharString(string first, string second)
+        {
+            if (String.IsNullOrEmpty(first))
+            {
+                throw new ArgumentException("First string is null or empty", nameof(first));
+            }
+
+            if (String.IsNullOrEmpty(second))
+            {
+                throw new ArgumentException("Second string is null or empty", nameof(second));
+            }
+
+            var hashSet = new HashSet<char>(second.Where(c => !Char.IsWhiteSpace(c)));
+            var sb = new StringBuilder();
+
+            foreach (var c in first)
+            {
+                sb.Append(hashSet.Contains(c) ? $"{c}{c}" : c.ToString());
+            }
+
+            return sb.ToString();
+        }
     }
 }
--- M03/Task1/M03_Task1/Program.cs
+++ M03/Task1/M03_Task1/Program.cs
@@ -4,7 +4,7 @@ namespace M03_Task1
 {
     internal class Program
     {
-        private static string avgLengthInputStr = "";
+        private static string avgLengthInputStr = "test test";
 
         static void Main(string[] args)
         {
@@ -12,6 +12,9 @@ namespace M03_Task1
             {
                 var avgLength = AvgWordLength.GetAvgWordLength(avgLengthInputStr);
                 Console.WriteLine($"Avg length: {avgLength}");
+
+                var doubledCharString = DoubleChars.GetDoubleCharString("omg i love shrek", "o kek");
+                Console.WriteLine($"Doubled string: {doubledCharString}");
             }
             catch (Exception ex)
             {
