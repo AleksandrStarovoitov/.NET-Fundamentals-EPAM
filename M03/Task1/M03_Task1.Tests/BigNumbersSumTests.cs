ae976c7145529f450e8a4616488db5ca0b516c62
Aleksandr Starovoytov <starovoytov.it@mail.ru>
2019-12-11 21:24:44 +0300

    Add unit tests for M03 task

---
+++ M03/Task1/M03_Task1.Tests/BigNumbersSumTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace M03_Task1.Tests
+{
+    [TestFixture]
+    public class BigNumbersSumTests
+    {
+        [Test]
+        public void Sum_EmptyStrings_ThrowsArgumentException()
+        {
+            var empty = String.Empty;
+            var nonEmpty = "a";
+
+            Assert.Multiple(() => 
+            {
+                Assert.That(() => BigNumbersSum.Sum(empty, empty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => BigNumbersSum.Sum(empty, nonEmpty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => BigNumbersSum.Sum(nonEmpty, empty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+            });
+        }
+
+        [Test]
+        public void Sum_NullStrings_ThrowsArgumentException()
+        {
+            string nullString = null;
+            var nonNullString = "a";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(() => BigNumbersSum.Sum(nullString, nullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => BigNumbersSum.Sum(nonNullString, nullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => BigNumbersSum.Sum(nullString, nonNullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+            });
+        }
+
+        [Test]
+        public void Sum_IncorrectNumbers_ThrowsArgumentException()
+        {
+            var correctNumber = "100";
+            var incorrectNumber = "1a00";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(() => BigNumbersSum.Sum(correctNumber, incorrectNumber),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo("Input string is not a correct number"));
+                Assert.That(() => BigNumbersSum.Sum(incorrectNumber, correctNumber),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo("Input string is not a correct number"));
+                Assert.That(() => BigNumbersSum.Sum(incorrectNumber, incorrectNumber),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo("Input string is not a correct number"));
+            });
+        }
+
+        [Test]
+        public void Sum_SmallNumbers1and1_Returns2()
+        {
+            var first = "1";
+            var second = "1";
+
+            var result = BigNumbersSum.Sum(first, second);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Sum_BigNumbers_ReturnsCorrectResult()
+        {
+            var first = "12250165209153784685228690083746175751559134784";
+            var second = "1225016520915378468522869008374617575155";
+
+            var result = BigNumbersSum.Sum(first, second);
+
+            Assert.That(result, Is.EqualTo("12250166434170305600607158606615184126176709939"));
+        }
+    }
+}
---
+++ M03/Task1/M03_Task1.Tests/DoubleCharsTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace M03_Task1.Tests
+{
+    [TestFixture]
+    public class DoubleCharsTests
+    {
+        [Test]
+        public void GetDoubleCharString_EmptyStrings_ThrowsArgumentException()
+        {
+            var empty = String.Empty;
+            var nonEmpty = "a";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(() => DoubleChars.GetDoubleCharString(empty, empty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => DoubleChars.GetDoubleCharString(empty, nonEmpty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => DoubleChars.GetDoubleCharString(nonEmpty, empty),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+            });
+        }
+
+        [Test]
+        public void GetDoubleCharString_NullStrings_ThrowsArgumentException()
+        {
+            string nullString = null;
+            var nonNullString = "a";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(() => DoubleChars.GetDoubleCharString(nullString, nullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => DoubleChars.GetDoubleCharString(nonNullString, nullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+                Assert.That(() => DoubleChars.GetDoubleCharString(nullString, nonNullString),
+                        Throws.ArgumentException.
+                        With.Message.
+                        EqualTo(Resources.Resources.NullOrEmptyExMessage));
+            });
+        }
+
+        [Test]
+        public void GetDoubleCharString_ValidStrings_ReturnsCorrectResult()
+        {
+            var first = "omg i love shrek";
+            var second = "o kek";
+
+            var result = DoubleChars.GetDoubleCharString(first, second);
+
+            Assert.That(result, Is.EqualTo("oomg i loovee shreekk"));
+        }
+
+        [Test]
+        public void GetDoubleCharString_NoOccurencies_ReturnsFirstString()
+        {
+            var first = "Test";
+            var second = "abcd";
+
+            var result = DoubleChars.GetDoubleCharString(first, second);
+
+            Assert.That(result, Is.EqualTo(first));
+        }
+    }    
+}
