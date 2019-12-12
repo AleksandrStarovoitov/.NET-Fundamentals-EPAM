643c7fe389bea35a05024db46b077eef1803befc
Aleksandr Starovoytov <starovoytov.it@mail.ru>
2019-12-12 11:56:18 +0300

    Add matrix sort, unit tests

--- M07/M07_Task.Tests/Properties/AssemblyInfo.cs
+++ M07/M07_Task.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("M07_Task")]
+[assembly: AssemblyTitle("M07_Task.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("M07_Task")]
+[assembly: AssemblyProduct("M07_Task.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1190b2f2-e8c9-4d31-b608-1624ebd5b948")]
+[assembly: Guid("69c2acda-be6b-466c-a473-361b1a5d3ab7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("M07_Task.Tests")]
--- M07/M07_Task/Program.cs
+++ M07/M07_Task/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace M07_Task
 {
@@ -10,6 +6,31 @@ namespace M07_Task
     {
         static void Main(string[] args)
         {
+            var array = new int[4][]
+            {
+                new int[4] { 1, 1, 1, 1 },
+                new int[4] { 2, 2, 2, 2 },
+                new int[4] { 4, 4, 1, 4 },
+                new int[4] { 3, 3, 3, 3 }
+            };
+
+            //in descending order of sums of matrix row elements
+            MatrixSort.Sort(array, (first, second) => first.Sum() < second.Sum());
+
+            //in ascending order of sums of matrix row elements
+            MatrixSort.Sort(array, (first, second) => first.Sum() > second.Sum());
+
+            //in descending order of maximum element in a matrix row;
+            MatrixSort.Sort(array, (first, second) => first.Max() < second.Max());
+
+            //in ascending order of maximum element in a matrix row;
+            MatrixSort.Sort(array, (first, second) => first.Max() > second.Max());
+
+            //in descending order of minimum element in a matrix row;
+            MatrixSort.Sort(array, (first, second) => first.Min() < second.Min());
+
+            //in ascending order of minimum element in a matrix row;
+            MatrixSort.Sort(array, (first, second) => first.Min() > second.Min());
         }
     }
 }
