bc434f2d7da1e5eb04cb6a6add4a708232dcce0f
Aleksandr Starovoytov <starovoytov.it@mail.ru>
2019-12-12 18:45:44 +0300

    Add task2

---
+++ M07/M07_Task2/Countdown.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace M07_Task2
+{
+    internal class Countdown
+    {
+        public event EventHandler SendMessage;
+
+        protected virtual void OnSendMessage()
+        {
+            SendMessage?.Invoke(this, null);
+        }
+
+        public void SendMessages()
+        {
+            OnSendMessage();
+        }
+    }
+}
---
+++ M07/M07_Task2/TestSubscriber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace M07_Task2
+{
+    abstract class TestSubscriber
+    {
+        private double waitSeconds;
+
+        public void Subscribe(Countdown cd, double waitSeconds)
+        {
+            if (cd == null)
+            {
+                throw new ArgumentNullException(nameof(cd));
+            }
+
+            cd.SendMessage += Cd_SendMessage;
+            this.waitSeconds = waitSeconds;
+        }
+
+        public void Unsubscribe(Countdown cd)
+        {
+            if (cd == null)
+            {
+                throw new ArgumentNullException(nameof(cd));
+            }
+
+            cd.SendMessage -= Cd_SendMessage;
+        }
+
+        private void Cd_SendMessage(object sender, EventArgs e)
+        {
+            var typeName = this.GetType().Name;
+
+            Console.WriteLine($"{typeName}. Waiting for {waitSeconds} seconds...");
+            Thread.Sleep(TimeSpan.FromSeconds(waitSeconds));
+            Console.WriteLine($"{typeName}. Finished waiting.");
+        }
+    }
+}
