9c683bf312d4903748275ffcddd1072e20efb201
Aleksandr Starovoytov <starovoytov.it@mail.ru>
2019-12-31 15:08:57 +0300

    Add search criteria processing (does not work yet)

--- M09/ConsoleApp/ConsoleApp/Program.cs
+++ M09/ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
 
@@ -11,10 +13,8 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            // -name Ivan -minmark 3 -maxmark 5 -datefrom 20/11/2012 -dateto 20/12/2012 -test Maths
-
-            var serializer = new JsonSerializer();
-
+            //TODO
+            var input = "-name Ivan -minmark 3 -maxmark 5 -datefrom 20/11/2012 -dateto 20/12/2012 -test Maths";
             var assembly = Assembly.GetExecutingAssembly();
 
             using (var stream =
@@ -23,31 +23,70 @@ namespace ConsoleApp
             {
                 var jsonString = stream.ReadToEnd();
 
-                var definition = new[]
-                {
-                    new { student = "", test = "", date = new DateTime(), mark = 0 }
-                };
-
-                var students = JsonConvert.DeserializeAnonymousType(jsonString, definition, new JsonSerializerSettings
+                var parsedStudents = JsonConvert.DeserializeObject<List<Student>>(jsonString, new JsonSerializerSettings
                 {
                     DateFormatString = "dd/mm/yyyy"
                 });
                 
                 Console.WriteLine("Hello! Please input your criteria:");
-
-                var input = Console.ReadLine();
+                
+                //TODO
+                Console.WriteLine(input); 
+                //var input = Console.ReadLine();
                 var splitInput = input.Split('-');
 
+                IEnumerable<Student> students = parsedStudents;
                 foreach (var flag in splitInput)
                 {
-                    ProcessSearchCriteria(flag);
+                    //TODO
+                    if (!flag.Any())
+                    {
+                        continue;
+                    }
+                    students = ProcessSearchCriteria(flag, students);
+                }
+
+                //TODO
+                Console.WriteLine("Student\tTest\tDate\tMark");
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine(student);
                 }
             }
         }
 
-        private static void ProcessSearchCriteria(string flag)
+        private static IEnumerable<Student> ProcessSearchCriteria(string flag, IEnumerable<Student> students)
         {
-            throw new NotImplementedException();
+            var flagValuePair = flag.Split(' ');
+
+            if (flagValuePair.Length < 2)
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+
+            switch (flagValuePair[0])
+            {
+                case "name":
+                    return students.Where(s => s.student.Equals(flagValuePair[1]));
+                case "minmark":
+                    var minMark = Int32.Parse(flagValuePair[1]);
+                    return students.Where(s => s.mark > minMark);
+                case "maxmark":
+                    var maxMark = Int32.Parse(flagValuePair[1]);
+                    return students.Where(s => s.mark < maxMark);
+                case "datefrom":
+                    //TODO datetime format
+                    var dateFrom = DateTime.Parse(flagValuePair[1]);
+                    return students.Where(s => s.date > dateFrom);
+                case "dateto":
+                    var dateTo = DateTime.Parse(flagValuePair[1]);
+                    return students.Where(s => s.date < dateTo);
+                case "test":
+                    return students.Where(s => s.test.Equals(flagValuePair[1]));
+                default:
+                    throw new ArgumentException("Invalid input.");
+            }
         }
     }
 }
---
+++ M09/ConsoleApp/ConsoleApp/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Student
+    {
+        public string student;
+        public string test;
+        public DateTime date;
+        public byte mark;
+
+        public override string ToString()
+        {
+            return $"{student}\t{test}\t{date}\t{mark}";
+        }
+    }
+}
