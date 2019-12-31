84b875eec543cca6276d1ce1d848d7e59ece93ac
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2019-12-31 19:31:18 +0300

    Complete tasks 1-5

--- M09/ConsoleApp/ConsoleApp/Program.cs
+++ M09/ConsoleApp/ConsoleApp/Program.cs
@@ -1,52 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
 namespace ConsoleApp
 {
-    class Program
+    public static class Program
     {
-        private const string jsonPath = "ConsoleApp.Resources.students.json";
+        private const string JsonPath = "ConsoleApp.Resources.students.json";
+        private const string DateFormat = "dd/MM/yyyy";
+        private const string InvalidInputEx = "Invalid input.";
+        private const string Name = "name", Mark = "mark", Test = "test", Date = "date";
 
         static void Main(string[] args)
         {
-            //TODO
-            var input = "-name Ivan -minmark 3 -maxmark 5 -datefrom 20/11/2012 -dateto 20/12/2012 -test Maths";
             var assembly = Assembly.GetExecutingAssembly();
 
             using (var stream =
-                new StreamReader(assembly.GetManifestResourceStream("ConsoleApp.Resources.students.json") ??
+                new StreamReader(assembly.GetManifestResourceStream(JsonPath) ??
                                  throw new InvalidOperationException()))
             {
                 var jsonString = stream.ReadToEnd();
 
                 var parsedStudents = JsonConvert.DeserializeObject<List<Student>>(jsonString, new JsonSerializerSettings
                 {
-                    DateFormatString = "dd/mm/yyyy"
+                    DateFormatString = DateFormat
                 });
-                
+
                 Console.WriteLine("Hello! Please input your criteria:");
-                
-                //TODO
-                Console.WriteLine(input); 
-                //var input = Console.ReadLine();
-                var splitInput = input.Split('-');
+
+                var input = Console.ReadLine();
+                var splitInput = input?.Split('-').Where(s => s.Any()) ?? throw new ArgumentException("No input");
+                var splitInputList = splitInput.ToList();
+
+                if (!splitInputList.Any())
+                {
+                    throw new ArgumentException("Invalid input");
+                }
 
                 IEnumerable<Student> students = parsedStudents;
-                foreach (var flag in splitInput)
+                foreach (var flag in splitInputList)
                 {
-                    //TODO
-                    if (!flag.Any())
-                    {
-                        continue;
-                    }
                     students = ProcessSearchCriteria(flag, students);
                 }
 
-                //TODO
                 Console.WriteLine("Student\tTest\tDate\tMark");
 
                 foreach (var student in students)
@@ -62,30 +63,80 @@ namespace ConsoleApp
 
             if (flagValuePair.Length < 2)
             {
-                throw new ArgumentException("Invalid input.");
+                throw new ArgumentException("Arguments number < 2");
             }
 
             switch (flagValuePair[0])
             {
-                case "name":
-                    return students.Where(s => s.student.Equals(flagValuePair[1]));
+                case Name:
+                    return students.Where(s => Regex.IsMatch(s.student, flagValuePair[1]));
+
                 case "minmark":
-                    var minMark = Int32.Parse(flagValuePair[1]);
-                    return students.Where(s => s.mark > minMark);
+                    if (Int32.TryParse(flagValuePair[1], out var minMark))
+                    {
+                        return students.Where(s => s.mark >= minMark);
+                    }
+                    throw new ArgumentException("Invalid mark value");
+
                 case "maxmark":
-                    var maxMark = Int32.Parse(flagValuePair[1]);
-                    return students.Where(s => s.mark < maxMark);
+                    if (Int32.TryParse(flagValuePair[1], out var maxMark))
+                    {
+                        return students.Where(s => s.mark <= maxMark);
+                    }
+                    throw new ArgumentException("Invalid mark value");
+
                 case "datefrom":
-                    //TODO datetime format
-                    var dateFrom = DateTime.Parse(flagValuePair[1]);
+                    var dateFrom = DateTime.ParseExact(flagValuePair[1], DateFormat, CultureInfo.InvariantCulture);
                     return students.Where(s => s.date > dateFrom);
+
                 case "dateto":
-                    var dateTo = DateTime.Parse(flagValuePair[1]);
+                    var dateTo = DateTime.ParseExact(flagValuePair[1], DateFormat, CultureInfo.InvariantCulture);
                     return students.Where(s => s.date < dateTo);
-                case "test":
+
+                case Test:
                     return students.Where(s => s.test.Equals(flagValuePair[1]));
+
+                case "sort":
+                    return ProcessSortCriteria(flagValuePair, students);
+
                 default:
-                    throw new ArgumentException("Invalid input.");
+                    throw new ArgumentException("Invalid search criteria");
+            }
+        }
+
+        private static IEnumerable<Student> ProcessSortCriteria(string[] flagValuePair, IEnumerable<Student> students)
+        {
+            if (flagValuePair.Length < 3)
+            {
+                throw new ArgumentException(InvalidInputEx);
+            }
+
+            const string asc = "asc", desc = "desc";
+
+            switch (flagValuePair[1])
+            {
+                case Name when flagValuePair[2].Equals(asc):
+                    return students.OrderBy(s => s.student);
+                case Name when flagValuePair[2].Equals(desc):
+                    return students.OrderByDescending(s => s.student);
+
+                case Mark when flagValuePair[2].Equals(asc):
+                    return students.OrderBy(s => s.mark);
+                case Mark when flagValuePair[2].Equals(desc):
+                    return students.OrderByDescending(s => s.mark);
+
+                case Test when flagValuePair[2].Equals(asc):
+                    return students.OrderBy(s => s.test);
+                case Test when flagValuePair[2].Equals(desc):
+                    return students.OrderByDescending(s => s.test);
+
+                case Date when flagValuePair[2].Equals(asc):
+                    return students.OrderBy(s => s.date);
+                case Date when flagValuePair[2].Equals(desc):
+                    return students.OrderByDescending(s => s.date);
+
+                default:
+                    throw new ArgumentException("Invalid sort criteria");
             }
         }
     }
--- M09/ConsoleApp/ConsoleApp/Student.cs
+++ M09/ConsoleApp/ConsoleApp/Student.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Student
+    internal class Student
     {
         public string student;
         public string test;
