9b80254648a543c3eeef67455589714fabb43ba5
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2020-02-09 17:48:56 +0300

    Remove unused using directives

--- module_20/WebApi/Startup.cs
+++ module_20/WebApi/Startup.cs
@@ -2,7 +2,6 @@ using ClassLibrary.BL.Interfaces;
 using ClassLibrary.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
